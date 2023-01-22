f510ee3a085be34e9f52c511d456f6ce41a97e99
Andrew <dickinsona19@yahoo.com>
2023-01-22 17:35:50 -0500

    Using Extract Delegate refactoring

--- src/InsuranceCalculator.java
+++ src/InsuranceCalculator.java
@@ -3,7 +3,15 @@ public class InsuranceCalculator {
     private final InsuranceStrategyVeryHigh insuranceStrategyVeryHigh = new InsuranceStrategyVeryHigh();
 
     public double calculateInsurance(double income) {
-        return insuranceStrategyVeryHigh.calculateInsurance(income);
+        if (income <= 10000) {
+            return income * 0.365;
+        } else if (income <= 30000) {
+            return (income - 10000) * 0.2 + 35600;
+        } else if (income <= 60000) {
+            return (income - 30000) * 0.1 + 76500;
+        } else {
+            return insuranceStrategyVeryHigh.calculateInsuranceVeryHigh(income);
+        }
     }
 
 
